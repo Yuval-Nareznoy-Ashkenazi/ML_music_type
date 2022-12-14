ff78503cc604d566db8961304be86a3d4a966c8b
Yuval-Nareznoy-Ashkenazi <yuval.ashkenazy1@gmail.com>
2022-12-14 08:39:44 +0200

    1.0.3

--- RealsedNotes.md
+++ RealsedNotes.md
@@ -1,5 +1,9 @@
 # ML-Project---Music Type - Versions
 
+# Version 1.0.3,  13-Dec-2022 #
+
+* Implementation of DecisionTreeClassifier
+
 # Version 1.0.2,  03-Dec-2022 #
 
 * Test (Yuval)
--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import pandas as pd
 import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import joblib
 
 
 def main():
     music_data = pd.read_csv('music.csv')
 
-#     cleaning the data
-    X = music_data.drop(columns = ['genre'])
-    print(X)
+#   data set up
+    X = music_data.drop(columns=['genre'])
+    y = music_data['genre']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8)
+
+#   training model
+    model = DecisionTreeClassifier()
+    model.fit(X_train.values, y_train)
+
+    joblib.dump(model, 'music-recommender.joblib')  # saving the trained model in a file
+
+    model = joblib.load('music-recommender.joblib')
+    predictions = model.predict(X_test)
+
+#   evaluate accuracy
+    score = accuracy_score(y_test, predictions)
+    print(score)
+
 
 if __name__ == '__main__':
     main()
